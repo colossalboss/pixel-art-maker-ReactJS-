0280927e958d5735eb270e592c7289b8986e648e
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2019-06-26 09:49:24 +0100

    Initialize repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greetings from './Greetings';
+import SimpleForm from './SimpleForm';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => (
+  <div className="App">
+    <h1>Hi, there!</h1>
+  </div>
+)
+
+
+export default App;
